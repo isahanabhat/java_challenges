c13149e56dab9315ab50a7a7f97db09fbd1d0afd
isahanabhat <isahanabhat@gmail.com>
2025-07-27 13:05:59 +0530

    start of java dict

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,22 @@ import java.io.File;
 import java.util.*;
 import java.util.Arrays;
 import java.util.Objects;
+import org.example.challenges.JavaDict;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String[] args_temp = {"java", "go"};
+        String[] args_temp = {"java", "go", "python"};
         Which which_cmd = new Which();
         ArrayList<String> result = which_cmd.find_location(args_temp);
         for (String r : result) {
             System.out.println(r);
         }
+        JavaDict j = new JavaDict();
+        // j.json_test();
+        j.put("sahana", "bhat");
+        j.put("hello", 5);
+        j.jsonToString();
     }
 }
---
+++ src/main/java/org/example/challenges/JavaDict.java
@@ -0,0 +1,44 @@
+package org.example.challenges;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/*
+Build a Java class that "mimics" the python dict data structure. Instead of [], use put/get methods.
+The final version of the dict must support multiple levels (nested) + different object types in the same dict. (similar to python dict).
+Weightage will be given to thought process about how to design the class.
+I want you to use your Java OCP knowledge and think of a sophisticated way to design so that your class is versatile.
+Start by building a simple prototype - of only 1 level and simple key/value pairs (i.e. no 2nd level / nested dicts/lists).
+Build iteratively and more and more complexity - 1 step at a time - as you'll likely do in a complex project setting.
+Use any JSON package library internally to speed up your work.
+I found com.google.gson a good one, but many people use jackson also.
+You'll need to select the one that supports generic objects (instead of just basic data types and strings).
+ */
+
+public class JavaDict {
+    public JSONArray jArray;
+    public JSONObject jObject;
+    public JavaDict() {
+        this.jArray = new JSONArray();
+        this.jObject = new JSONObject();
+    }
+    
+    public void json_test() {
+        JSONObject jo = new JSONObject();
+        jo.put("name", "jon doe");
+        jo.put("age", "22");
+        jo.put("city", 2);
+        System.out.println(jo.toString());
+        jArray.put(Boolean.TRUE);
+        jArray.put("lorem ipsum");
+        jArray.put(jo);
+        System.out.println(jArray.toString(4));
+    }
+    
+    public void put(Object key, Object value) {
+        jObject.put((String) key, value);
+    }
+    public void jsonToString() {
+        System.out.println(jObject.toString(4));
+    }
+}
